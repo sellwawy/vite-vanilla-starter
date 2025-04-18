4101d4bfbb14c183f2dac347e49d794733b68435
Mohamed Mahmoud <blackbird2013.44@gmail.com>
2025-04-18 22:28:12 +0200

    Add Autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // /** @type {import('vite').UserConfig} */
 import { resolve } from 'path'
 import pxtorem from 'postcss-pxtorem'
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -23,11 +24,14 @@ export default defineConfig({
       devSourcemap: true,
       preprocessorOptions: {
          scss: {
-            api: 'modern-compiler', // or "modern"
+            api: 'modern-compiler',
          },
       },
       postcss: {
-         plugins: [pxtorem({ propList: process.env.NODE_ENV === 'development' ? ['!*'] : ['*'] })],
+         plugins: [
+            autoprefixer(),
+            pxtorem({ propList: process.env.NODE_ENV === 'development' ? ['!*'] : ['*'] }),
+         ],
       },
    },
 })
